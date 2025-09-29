26b083191dbcf980ebc428be9191793530587445
MinhDucHo <minhducmediallc@gmail.com>
2025-09-29 16:57:05 -0500

    build: simplify Babel (remove transform-remove-console)

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,23 @@
-module.exports = function(api) {
+// babel.config.js
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // Keep aliases for "@/..." imports. Use the short plugin name "module-resolver".
     plugins: [
-      ['@babel/plugin-transform-runtime'],
-      ['module-resolver', {
-        root: ['./src'],
-        alias: { '@': './src' }
-      }]
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+          },
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+      ],
+      // Reanimated plugin must be last if you use reanimated
+      'react-native-reanimated/plugin',
     ],
-    env: {
-      production: {
-        plugins: [['transform-remove-console', { exclude: ['error', 'warn'] }]]
-      }
-    }
+    // If you previously added any custom "unstable_transformImportMeta" flag, remove it now.
   };
 };
